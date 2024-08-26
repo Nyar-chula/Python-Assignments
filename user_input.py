f9baad1a7420f4fe7c14a943a6968d26b12ad538
Nyar-chula <aokopauline@gmail.com>
2024-08-26 16:03:39 +0300

    This is the first assignment

---
+++ user_input.py
@@ -0,0 +1,7 @@
+
+#user_input.py
+# code on how to take user input
+name = input ("What is your name?")
+age = input ("How old are you ?")
+location= input ("Where do you live?")
+print (f"Hello {name}, you are {age} years old and live in {location}.")
