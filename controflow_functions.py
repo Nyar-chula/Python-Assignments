7c97cdf9b7f5054a5d97c7c0af03aafad055bec6
Nyar-chula <aokopauline@gmail.com>
2024-09-17 13:26:09 +0300

    updated assignments

---
+++ controflow_functions.py
@@ -0,0 +1,10 @@
+# large power
+def large_power (base,exponent):
+# calculate the result of the base to the power of exponent
+    result= base ** exponent
+    if result>5000:
+        return ("True")
+    else:
+        return ("False")    
+
+    
